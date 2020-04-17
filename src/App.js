3269c419188be96f9b4804af7c48739548a7d2d0
schvbertt <diamenttx@gmail.com>
2020-04-17 17:43:37 +0200

    Prototype

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+// import cartItems from "./cart-items";
+// redux stuff
+
+import {createStore} from "redux";
+import reducer from "./reducer"
+// react-redux - Provider - wraps app , conntect - used in components
+import {Provider} from 'react-redux';
+// initial store
+// const initialStore = {
+//   cart: cartItems,
+//   total: 0,
+//   amount: 0
+// }
+
+// store
+const store = createStore(reducer);
+
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+      </Provider>
+    </main>
+  );
+}
+
+export default App;
