f2279b0efc28d6703dcbbc0f7bc4741557d5240c
schvbertt <diamenttx@gmail.com>
2020-04-25 13:32:33 +0200

    Updates

---
+++ precache-manifest.de96e9ce25f957ec6703ab9164826d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8bd88fca5ed665c880c250368420b1",
+    "url": "/redux-cart/index.html"
+  },
+  {
+    "revision": "2a8b215e23cc95c5d839",
+    "url": "/redux-cart/static/css/main.c463a086.chunk.css"
+  },
+  {
+    "revision": "37815099db1789d0f5d7",
+    "url": "/redux-cart/static/js/2.2c877b7b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/redux-cart/static/js/2.2c877b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8b215e23cc95c5d839",
+    "url": "/redux-cart/static/js/main.ec60d267.chunk.js"
+  },
+  {
+    "revision": "31bbde52713eafd3bce7",
+    "url": "/redux-cart/static/js/runtime-main.5bf13f75.js"
+  }
+]);
